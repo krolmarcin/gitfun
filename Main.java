39179dc917de491e14f419fa8f95b575a31aaede
Marcin <szeryfmk@gmail.com>
2017-04-08 15:30:16 +0200

    bar method

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello GIT!!!!!!!!");
-        System.out.println("Oh yeaaa tra la la");
+        System.out.println("Hello GIYES");
+        System.out.println("Oh yeaaa tra la la i costam");
 		
     }
 
