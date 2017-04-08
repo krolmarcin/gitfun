29eba4088e10a8332033a9e263aa8f8ee9508fd7
Marcin <szeryfmk@gmail.com>
2017-04-08 15:29:05 +0200

    zmiany

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello GIT!!!!!!!!");
-        System.out.println("Oh yeaaa oh yeah tra la la!!!!");
+        System.out.println("Hello GIT");
+        System.out.println("Tra la la");
 		
     }
 
